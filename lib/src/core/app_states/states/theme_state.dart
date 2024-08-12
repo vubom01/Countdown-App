4e11f26ed4dcac6ce65ea3fa1d9a94bede38bad2
vu.lh <vu.lh@teko.vn>
2024-08-12 10:43:58 +0700

    update primary color

--- lib/src/core/app_states/states/theme_state.dart
+++ lib/src/core/app_states/states/theme_state.dart
@@ -6,8 +6,8 @@ abstract class ThemeState extends ChangeNotifier {
   static ThemeState get to => AppInjector.injector<ThemeState>();
 
   static final Map<ThemeMode, ThemeData> themes = {
-    ThemeMode.dark: TekThemes.dark,
     ThemeMode.light: TekThemes.light,
+    ThemeMode.dark: TekThemes.dark,
   };
 
   ThemeState init();
